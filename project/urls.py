5c094d87d14db6e9ef68c30f180a658bb5b4382d
Nate Moseley <FinalDoom@gmail.com>
2016-10-16 18:18:08 -0400

    Missed the url addition. Sigh.

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from welcome.views import index, health
+import website
 
 urlpatterns = [
     # Examples:
@@ -11,4 +12,5 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^website/', include(website.index)),
 ]
